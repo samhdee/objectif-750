6527e4b278991da778ca534809b7c30b08c9f88e
claradhaleine <clara.dhaleine@gmail.com>
2017-10-23 00:25:34 +0200

    Insights

---
+++ src/UserBundle/Form/Type/AvatarType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class AvatarType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+      $builder->add(
+        'file',
+        FileType::class,
+        array(
+          'required' => false,
+          'label' => 'profile.upload.avatar'
+        )
+      );
+
+      // $builder->addEventListener(
+      // FormEvents::PRE_SET_DATA,
+      // function(FormEvent $event) {
+      //   $groupe = $event->getData();
+
+      //   if (null === $groupe) {
+      //     return;
+      //   }
+      // });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\Avatar'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_avatar';
+    }
+}
---
+++ src/UserBundle/Form/Type/ProfileType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use UserBundle\Form\AvatarType;
+use UserBundle\Form\UserPreferencesType;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('avatar', AvatarType::class, array('required' => false));
+        $builder->add('user_preferences', UserPreferencesType::class);
+        $builder->remove('current_password');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
