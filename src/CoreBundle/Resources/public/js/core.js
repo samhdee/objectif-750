e5877e3720d195dd2575944acc9c3af53da515c7
claradhaleine <clara.dhaleine@gmail.com>
2017-12-04 17:55:51 +0100

    Traduction en anglais (2/?)

--- src/CoreBundle/Resources/public/js/core.js
+++ src/CoreBundle/Resources/public/js/core.js
@@ -1,5 +1,11 @@
 ( function($) {
   $(document).ready(function() {
+    $('#switch_languages a#current').on('click', function() {
+      $('#switch_languages a#switch_to').toggle(400);
+      $('#switch_languages a#current span.downward_arrow').toggle(400);
+      $('#switch_languages a#current span.upward_arrow').toggle(400);
+    });
+
     $('nav.navbar ul#main_menu li.first_level').on('mouseover', function() {
       $(this).find('ul.submenu').fadeIn('fast');
       $(this).find('a.first_level_link').trigger('hover');
